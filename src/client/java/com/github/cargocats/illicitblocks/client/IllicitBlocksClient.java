b057c878a1c197221b99908ab62b36ed08ceb808
cargocats <ceggy@protonmail.com>
2025-06-07 22:10:53 -0400

    Add keybind for dumping identifiers to static_list

--- src/client/java/com/github/cargocats/illicitblocks/client/IllicitBlocksClient.java
+++ src/client/java/com/github/cargocats/illicitblocks/client/IllicitBlocksClient.java
@@ -1,10 +1,43 @@
 package com.github.cargocats.illicitblocks.client;
 
+import com.github.cargocats.illicitblocks.ConfigManager;
+import com.github.cargocats.illicitblocks.IllicitBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class IllicitBlocksClient implements ClientModInitializer {
+    public static final int DUMP_KEYCODE = GLFW.GLFW_KEY_RIGHT_BRACKET;
+    private static boolean wasPressed = false;
 
     @Override
     public void onInitializeClient() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client == null || client.getWindow() == null) return;
+
+            long windowHandle = client.getWindow().getHandle();
+            boolean isDown = GLFW.glfwGetKey(windowHandle, DUMP_KEYCODE) == GLFW.GLFW_PRESS;
+
+            if (isDown && !wasPressed) {
+                dumpBlocks(client);
+            }
+
+            wasPressed = isDown;
+        });
+    }
+
+    private void dumpBlocks(MinecraftClient client) {
+        ConfigManager.config.static_list =
+                IllicitBlocks.blocksToHandle.stream().map(Identifier::toString)
+                        .collect(Collectors.toCollection(ArrayList::new));
+
+        ConfigManager.saveConfig();
+
+        IllicitBlocks.LOG.info("Dumped blocks to static_list for config");
     }
 }
