b390cea3ab7ee67646b43323d5697c11d5502a51
cargocats <ceggy@protonmail.com>
2025-06-07 22:09:27 -0400

    New config settings

--- src/main/java/com/github/cargocats/illicitblocks/Config.java
+++ src/main/java/com/github/cargocats/illicitblocks/Config.java
@@ -3,5 +3,9 @@ package com.github.cargocats.illicitblocks;
 import java.util.ArrayList;
 
 public class Config {
-    public ArrayList<String> ignored_identifiers = new ArrayList<>();
+    public ArrayList<String> excluded_identifiers = new ArrayList<>();
+    public ArrayList<String> excluded_namespaces = new ArrayList<>();
+    public ArrayList<String> included_identifiers = new ArrayList<>();
+    public boolean use_static_list = false;
+    public ArrayList<String> static_list = new ArrayList<>();
 }
