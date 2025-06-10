ea22ee8e30e66f5c2f79096a7f1157b8aa93b397
cargocats <ceggy@protonmail.com>
2025-06-10 18:13:42 -0400

    Refactor code

--- src/client/java/com/github/cargocats/illicitblocks/client/IllicitBlocksClient.java
+++ src/client/java/com/github/cargocats/illicitblocks/client/IllicitBlocksClient.java
@@ -2,87 +2,212 @@ package com.github.cargocats.illicitblocks.client;
 
 import com.github.cargocats.illicitblocks.ConfigManager;
 import com.github.cargocats.illicitblocks.IllicitBlocks;
-import com.github.cargocats.illicitblocks.Utils;
+import com.github.cargocats.illicitblocks.client.api.AdditionalItemAssetRegistrationCallback;
+import com.github.cargocats.illicitblocks.client.api.AdditionalModelRegistrationCallback;
 import com.google.gson.JsonObject;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.FlowerPotBlock;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.block.WallBannerBlock;
+import net.minecraft.block.WallHangingSignBlock;
+import net.minecraft.block.WallSignBlock;
+import net.minecraft.block.WallSkullBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.ItemAsset;
 import net.minecraft.client.render.item.model.BasicItemModel;
+import net.minecraft.client.render.item.model.SpecialItemModel;
+import net.minecraft.client.render.item.model.special.BannerModelRenderer;
+import net.minecraft.client.render.item.model.special.HeadModelRenderer;
+import net.minecraft.client.render.item.tint.ConstantTintSource;
+import net.minecraft.client.render.item.tint.TintSource;
 import net.minecraft.client.render.model.json.JsonUnbakedModel;
+import net.minecraft.client.toast.SystemToast;
 import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import org.lwjgl.glfw.GLFW;
+import net.minecraft.util.math.ColorHelper;
 
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.github.cargocats.illicitblocks.IllicitBlocks.*;
 
 public class IllicitBlocksClient implements ClientModInitializer {
-    public static final int DUMP_KEYCODE = GLFW.GLFW_KEY_RIGHT_BRACKET;
-    private static boolean wasPressed = false;
-
     @Override
     public void onInitializeClient() {
-        handleKeybind();
-
         AdditionalItemAssetRegistrationCallback.EVENT.register(context -> {
-            IllicitBlocks.blocksToHandle.forEach(id -> {
-                if (context.hasAsset(id)) return;
+            moddedBlocks.forEach(id -> addItemAssetForId(context, id));
 
-                context.addAsset(id, new ItemAsset(
-                        new BasicItemModel.Unbaked(
-                                id,
-                                List.of()
-                        ),
-                        ItemAsset.Properties.DEFAULT
-                ));
+            createdBlockItems.forEach(id -> {
+                if (id.getNamespace().equals("minecraft")) {
+                    addItemAssetForId(context, id);
+                }
             });
         });
 
         AdditionalModelRegistrationCallback.EVENT.register(context -> {
-            IllicitBlocks.blocksToHandle.forEach(id -> {
-                if (context.hasModel(id)) return;
+            moddedBlocks.forEach(id -> addModelForId(context, id));
 
-                JsonObject jsonRoot = new JsonObject();
-                jsonRoot.addProperty("parent", "block/" + id.getPath());
-                jsonRoot.addProperty("gui_light", "front");
-
-                context.addModel(id, JsonUnbakedModel.deserialize(new StringReader(jsonRoot.toString())));
+            createdBlockItems.forEach(id -> {
+                if (id.getNamespace().equals("minecraft")) {
+                    addModelForId(context, id);
+                }
             });
         });
-    }
 
-    private void handleKeybind() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            if (client == null || client.getWindow() == null) return;
-
-            long windowHandle = client.getWindow().getHandle();
-            boolean isDown = GLFW.glfwGetKey(windowHandle, DUMP_KEYCODE) == GLFW.GLFW_PRESS;
-
-            if (isDown && !wasPressed) {
-                dumpBlocks(client);
-            }
-
-            wasPressed = isDown;
-        });
+        ClientLifecycleEvents.CLIENT_STARTED.register(this::dumpBlocks);
     }
 
     private void dumpBlocks(MinecraftClient client) {
-        ArrayList<Identifier> blocksToHandle = new ArrayList<>(IllicitBlocks.blocksToHandle);
-        blocksToHandle.removeIf(blockId -> {
-            boolean containedId = Registries.ITEM.containsId(blockId);
-            Utils.debugLog("Block {} contained id now? {}", blockId, containedId);
-            return containedId;
+        if (!ConfigManager.config.create_list_after_freeze) return;
+
+        ArrayList<String> moddedBlockList = ConfigManager.config.modded_block_list;
+        AtomicBoolean newBlock = new AtomicBoolean(false);
+
+        Registries.BLOCK.forEach(block -> {
+            Identifier blockId = Registries.BLOCK.getId(block);
+
+            if (blockId.getNamespace().equals("minecraft")) return;
+
+            if (block != Blocks.AIR && !Registries.ITEM.containsId(blockId) && shouldHandleBlock(blockId)) {
+                if (!moddedBlockList.contains(blockId.toString())) {
+                    newBlock.set(true);
+                }
+                moddedBlockList.add(blockId.toString());
+            }
         });
 
-        ConfigManager.config.static_list =
-                blocksToHandle.stream().map(Identifier::toString)
-                        .collect(Collectors.toCollection(ArrayList::new));
+        if (!newBlock.get()) {
+            IllicitBlocks.LOG.info("No new blocks found");
+            return;
+        }
 
+        ConfigManager.config.modded_block_list = moddedBlockList;
         ConfigManager.saveConfig();
 
-        IllicitBlocks.LOG.info("Dumped blocks to static_list for config, restart minecraft");
+        client.getToastManager().add(
+                new SystemToast(
+                        new SystemToast.Type(10000),
+                        Text.translatable("illicitblocks.mod_name"),
+                        Text.translatable("illicitblocks.toast.restart")
+                )
+        );
+
+        IllicitBlocks.LOG.info("Dumped modded blocks to block_list for config, restart minecraft");
+    }
+
+    public static void addItemAssetForId(AdditionalItemAssetRegistrationCallback.Context context, Identifier id) {
+        if (context.hasAsset(id)) return;
+        Block block = Registries.BLOCK.get(id);
+
+        List<TintSource> tintSources = new ArrayList<>();
+
+        if (id.equals(Identifier.ofVanilla("water"))) {
+            tintSources.add(new ConstantTintSource(ColorHelper.getArgb(63, 118, 228)));
+        }
+
+        if (block instanceof WallBannerBlock wallBannerBlock) {
+            context.addAsset(id, new ItemAsset(
+                    new SpecialItemModel.Unbaked(
+                            Identifier.ofVanilla("item/template_banner"),
+                            new BannerModelRenderer.Unbaked(wallBannerBlock.getColor())
+                    ),
+                    ItemAsset.Properties.DEFAULT
+            ));
+            return;
+        }
+
+        if (block instanceof WallSkullBlock wallSkullBlock) {
+            context.addAsset(id, new ItemAsset(
+                    new SpecialItemModel.Unbaked(
+                            Identifier.ofVanilla("item/template_skull"),
+                            new HeadModelRenderer.Unbaked(wallSkullBlock.getSkullType())
+                    ),
+                    ItemAsset.Properties.DEFAULT
+            ));
+            return;
+        }
+
+        context.addAsset(id, new ItemAsset(
+                new BasicItemModel.Unbaked(
+                        id,
+                        tintSources
+                ),
+                ItemAsset.Properties.DEFAULT
+        ));
+    }
+
+    public static void addModelForId(AdditionalModelRegistrationCallback.Context context, Identifier id) {
+        if (context.hasModel(id)) return;
+        Block block = Registries.BLOCK.get(id);
+
+        JsonObject jsonRoot = new JsonObject();
+        jsonRoot.addProperty("gui_light", "front");
+
+        if (block instanceof FluidBlock) {
+            jsonRoot.addProperty("parent", "minecraft:item/generated");
+
+            JsonObject textures = new JsonObject();
+            textures.addProperty("layer0", id.getNamespace() + ":block/" + id.getPath() + "_still");
+            jsonRoot.add("textures", textures);
+        } else if (block instanceof CropBlock) {
+            jsonRoot.addProperty("parent", "block/" + id.getPath() + "_stage0");
+        } else if (block instanceof WallSignBlock wallSignBlock) {
+            jsonRoot.addProperty("parent", "minecraft:item/generated");
+
+            JsonObject textures = new JsonObject();
+            String woodName = wallSignBlock.getWoodType().name().replaceFirst(id.getNamespace() + ":", "");
+            if (id.getNamespace().equals("terrestria")) {
+                woodName = extractWoodType(id.toString());
+            }
+
+            String itemTextureString = id.getNamespace() + ":item/" + woodName + "_sign";
+            textures.addProperty("layer0", itemTextureString);
+            jsonRoot.add("textures", textures);
+        } else if (block instanceof WallHangingSignBlock wallHangingSignBlock) {
+            jsonRoot.addProperty("parent", "minecraft:item/generated");
+
+            JsonObject textures = new JsonObject();
+            String woodName = wallHangingSignBlock.getWoodType().name().replaceFirst(id.getNamespace() + ":", "");
+            if (id.getNamespace().equals("terrestria")) {
+                woodName = extractWoodType(id.toString());
+            }
+
+            textures.addProperty("layer0", id.getNamespace() + ":item/" + woodName + "_hanging_sign");
+            jsonRoot.add("textures", textures);
+        } else if (block instanceof FlowerPotBlock flowerPotBlock && !id.getNamespace().equals("minecraft")) {
+            jsonRoot.addProperty("parent", "minecraft:block/flower_pot_cross");
+
+            JsonObject textures = new JsonObject();
+            textures.addProperty("plant", id.getNamespace() + ":block/" + id.getPath().replace("potted_", ""));
+            jsonRoot.add("textures", textures);
+        }  else {
+            jsonRoot.addProperty("parent", "block/" + id.getPath());
+        }
+
+        context.addModel(id, JsonUnbakedModel.deserialize(new StringReader(jsonRoot.toString())));
+    }
+
+    public static String extractWoodType(String fullId) {
+        int colonIndex = fullId.indexOf(":");
+        String path = colonIndex >= 0 ? fullId.substring(colonIndex + 1) : fullId;
+
+        if (path.endsWith(":")) {
+            path = path.substring(0, path.length() - 1);
+        }
+
+        if (path.endsWith("_wall_hanging_sign")) {
+            path = path.substring(0, path.length() - "_wall_hanging_sign".length());
+        } else if (path.endsWith("_wall_sign")) {
+            path = path.substring(0, path.length() - "_wall_sign".length());
+        }
+
+        return path;
     }
 }
--- src/main/java/com/github/cargocats/illicitblocks/IllicitBlocks.java
+++ src/main/java/com/github/cargocats/illicitblocks/IllicitBlocks.java
@@ -2,12 +2,11 @@ package com.github.cargocats.illicitblocks;
 
 import com.github.cargocats.illicitblocks.item.BlockStateBlockItem;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.BlockItem;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -22,80 +21,99 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 public class IllicitBlocks implements ModInitializer {
     public static final String MOD_ID = "illicitblocks";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
-    public static final ArrayList<Identifier> blocksToHandle = new ArrayList<>();
-    public static boolean DEBUG_LOGGING;
+    public static ArrayList<Identifier> moddedBlocks = new ArrayList<>();
+    public static ArrayList<Identifier> createdBlockItems = new ArrayList<>();
+    public static boolean DEBUG_LOGGING = true;
 
     public static final RegistryKey<ItemGroup> ILLICIT_BLOCKS_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(MOD_ID, "illicitblocks_item_group"));
     public static final ItemGroup ILLICIT_BLOCKS_ITEM_GROUP = FabricItemGroup.builder()
             .icon(() -> new ItemStack(Items.BARRIER))
-            .displayName(Text.literal("Illicit Blocks"))
+            .displayName(Text.translatable("illicitblocks.item_group"))
             .build();
 
     @Override
     public void onInitialize() {
         ConfigManager.loadConfig();
         DEBUG_LOGGING = ConfigManager.config.debug;
+        moddedBlocks = ConfigManager.config.modded_block_list.stream()
+                .map(Identifier::of)
+                .collect(Collectors.toCollection(ArrayList::new));
 
         Registry.register(Registries.ITEM_GROUP, ILLICIT_BLOCKS_ITEM_GROUP_KEY, ILLICIT_BLOCKS_ITEM_GROUP);
 
-        RegistryEntryAddedCallback.event(Registries.BLOCK).register((rawId, id, block) -> handleBlock(block));
-        Registries.BLOCK.forEach(this::handleBlock);
+        ArrayList<BlockStateBlockItem> blockItems = registerBlockItems();
+        ItemGroupEvents.modifyEntriesEvent(ILLICIT_BLOCKS_ITEM_GROUP_KEY)
+                .register(group -> blockItems.forEach(group::add));
 
         LOG.info("Loaded Illicit Blocks");
     }
 
-    private void handleBlock(Block block) {
-        Identifier blockId = Registries.BLOCK.getId(block);
+    private ArrayList<BlockStateBlockItem> registerBlockItems() {
+        ArrayList<BlockStateBlockItem> collected = new ArrayList<>();
+        AtomicBoolean hasModdedContent = new AtomicBoolean(false);
 
-        if (!shouldHandleBlock(blockId)) {
-            return;
-        }
-
-        Utils.debugLog(
-                "Attempt to handle block id: {}, item form: {}, exists in item reg: {}",
-                blockId,
-                block.asItem(),
-                Registries.ITEM.containsId(blockId)
-        );
-
-        if (block != Blocks.AIR && !Registries.ITEM.containsId(blockId)) {
-            if (ConfigManager.config.register_block_items) {
-                BlockItem blockItem = new BlockStateBlockItem(block, new Item.Settings()
-                        .registryKey(RegistryKey.of(RegistryKeys.ITEM, Registries.BLOCK.getId(block)))
-                        .useBlockPrefixedTranslationKey()
-                );
-
-                Registry.register(Registries.ITEM, Registries.BLOCK.getId(block), blockItem);
-                ItemGroupEvents.modifyEntriesEvent(ILLICIT_BLOCKS_ITEM_GROUP_KEY).register(itemGroup -> itemGroup.add(blockItem));
+        Registries.BLOCK.forEach(block -> {
+            Identifier id = Registries.BLOCK.getId(block);
+            if ("minecraft".equals(id.getNamespace())) {
+                BlockStateBlockItem item = tryRegisterBlock(id);
+                if (item != null) collected.add(item);
+            } else {
+                hasModdedContent.set(true);
             }
+        });
 
-            Utils.debugLog("Adding in illicit block for block {}", block);
-
-            blocksToHandle.add(blockId);
+        for (Identifier id : moddedBlocks) {
+            LOG.info("Modded block {}", id);
+            BlockStateBlockItem item = tryRegisterBlock(id);
+            LOG.info("BLockstateblockitem {}", item);
+            if (item != null) collected.add(item);
         }
+
+        if (hasModdedContent.get() && moddedBlocks.isEmpty()) {
+            ItemStack placeholder = new ItemStack(Items.BARRIER);
+            placeholder.set(DataComponentTypes.ITEM_NAME, Text.translatable("illicitblocks.placeholder_description"));
+            ItemGroupEvents.modifyEntriesEvent(ILLICIT_BLOCKS_ITEM_GROUP_KEY)
+                    .register(group -> group.add(placeholder));
+        }
+
+        return collected;
+    }
+
+    private BlockStateBlockItem tryRegisterBlock(Identifier id) {
+        if (!shouldHandleBlock(id)) {
+            Utils.debugLog("Do not handle block {}", id);
+            return null;
+        }
+
+        Block block = Registries.BLOCK.get(id);
+        BlockStateBlockItem blockStateBlockItem = new BlockStateBlockItem(block, new Item.Settings()
+                .registryKey(RegistryKey.of(RegistryKeys.ITEM, id))
+                .useBlockPrefixedTranslationKey());
+
+        Registry.register(Registries.ITEM, id, blockStateBlockItem);
+        createdBlockItems.add(id);
+
+        Utils.debugLog("Registered block item: {}", id);
+        return blockStateBlockItem;
     }
 
     public static boolean shouldHandleBlock(Identifier blockId) {
-        if ((
-                ConfigManager.config.excluded_identifiers.contains(blockId.toString()) || ConfigManager.config.excluded_namespaces.contains(blockId.getNamespace()))
-                && !ConfigManager.config.included_identifiers.contains(blockId.toString())
-        ) {
-            Utils.debugLog("Ignoring block {}", blockId);
+        if (ConfigManager.config.included_identifiers.contains(blockId.toString())) {
+            Utils.debugLog("In included identifiers, returning true for {}", blockId);
+            return true;
+        }
+
+        if (ConfigManager.config.excluded_identifiers.contains(blockId.toString()) || ConfigManager.config.excluded_namespaces.contains(blockId.getNamespace())) {
+            Utils.debugLog("In excluded identifiers OR namespace, returning false for {}", blockId);
             return false;
         }
 
-        if (
-                ConfigManager.config.use_static_list && !ConfigManager.config.static_list.contains(blockId.toString())
-                        && !ConfigManager.config.included_identifiers.contains(blockId.toString())
-        ) {
-            Utils.debugLog("Ignoring block because not in static and not in included identifiers {}", blockId);
-            return false;
-        }
-
-        return true;
+        return Registries.BLOCK.get(blockId) != Blocks.AIR && !Registries.ITEM.containsId(blockId);
     }
 }
