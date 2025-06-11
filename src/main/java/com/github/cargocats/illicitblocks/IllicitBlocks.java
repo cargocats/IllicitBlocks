8741baf19aaea73c0eb9f6fa018ef1fe079b43ca
cargocats <ceggy@protonmail.com>
2025-06-11 15:59:53 -0400

    Generate all block states, except for some properties to our block items

--- src/main/java/com/github/cargocats/illicitblocks/IllicitBlocks.java
+++ src/main/java/com/github/cargocats/illicitblocks/IllicitBlocks.java
@@ -1,6 +1,6 @@
 package com.github.cargocats.illicitblocks;
 
-import com.github.cargocats.illicitblocks.item.BlockStateBlockItem;
+import com.github.cargocats.illicitblocks.item.IllicitBlockItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
@@ -8,20 +8,28 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.BlockStateComponent;
+import net.minecraft.component.type.NbtComponent;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
@@ -30,6 +38,7 @@ public class IllicitBlocks implements ModInitializer {
     public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
     public static ArrayList<Identifier> moddedBlocks = new ArrayList<>();
     public static ArrayList<Identifier> createdBlockItems = new ArrayList<>();
+    public static final Set<String> ignoredProperties = Set.of("facing", "horizontal_facing", "vertical_direction", "axis", "rotation", "east", "west", "south", "north");
     public static boolean DEBUG_LOGGING = true;
 
     public static final RegistryKey<ItemGroup> ILLICIT_BLOCKS_ITEM_GROUP_KEY = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(MOD_ID, "illicitblocks_item_group"));
@@ -48,15 +57,18 @@ public class IllicitBlocks implements ModInitializer {
 
         Registry.register(Registries.ITEM_GROUP, ILLICIT_BLOCKS_ITEM_GROUP_KEY, ILLICIT_BLOCKS_ITEM_GROUP);
 
-        ArrayList<BlockStateBlockItem> blockItems = registerBlockItems();
+        ArrayList<IllicitBlockItem> blockItems = registerBlockItems();
         ItemGroupEvents.modifyEntriesEvent(ILLICIT_BLOCKS_ITEM_GROUP_KEY)
-                .register(group -> blockItems.forEach(group::add));
+                .register(group -> blockItems.forEach(blockItem -> {
+                    group.add(blockItem);
+                    generateItemStacksFromBlockItem(blockItem).forEach(group::add);
+                }));
 
         LOG.info("Loaded Illicit Blocks");
     }
 
-    private ArrayList<BlockStateBlockItem> registerBlockItems() {
-        ArrayList<BlockStateBlockItem> collected = new ArrayList<>();
+    private ArrayList<IllicitBlockItem> registerBlockItems() {
+        ArrayList<IllicitBlockItem> collected = new ArrayList<>();
         AtomicBoolean hasModdedContent = new AtomicBoolean(false);
 
         RegistryEntryAddedCallback.allEntries(Registries.BLOCK, refBlock -> {
@@ -64,11 +76,11 @@ public class IllicitBlocks implements ModInitializer {
             Identifier id = Registries.BLOCK.getId(block);
 
             if ("minecraft".equals(id.getNamespace())) {
-                BlockStateBlockItem item = tryRegisterBlock(id);
+                IllicitBlockItem item = tryRegisterBlock(id);
                 if (item != null) collected.add(item);
             } else {
                 if (moddedBlocks.contains(id)) {
-                    BlockStateBlockItem item = tryRegisterBlock(id);
+                    IllicitBlockItem item = tryRegisterBlock(id);
                     if (item != null) collected.add(item);
                 }
                 hasModdedContent.set(true);
@@ -85,14 +97,14 @@ public class IllicitBlocks implements ModInitializer {
         return collected;
     }
 
-    private BlockStateBlockItem tryRegisterBlock(Identifier id) {
+    private IllicitBlockItem tryRegisterBlock(Identifier id) {
         if (!shouldHandleBlock(id)) {
             Utils.debugLog("Do not handle block {}", id);
             return null;
         }
 
         Block block = Registries.BLOCK.get(id);
-        BlockStateBlockItem blockStateBlockItem = new BlockStateBlockItem(block, new Item.Settings()
+        IllicitBlockItem blockStateBlockItem = new IllicitBlockItem(block, new Item.Settings()
                 .registryKey(RegistryKey.of(RegistryKeys.ITEM, id))
                 .useBlockPrefixedTranslationKey());
 
@@ -101,6 +113,31 @@ public class IllicitBlocks implements ModInitializer {
 
         Utils.debugLog("Registered block item: {}", id);
         return blockStateBlockItem;
+    }
+
+    private List<ItemStack> generateItemStacksFromBlockItem(BlockItem blockItem) {
+        return blockItem.getBlock().getStateManager().getStates().stream()
+                .map(state -> {
+                    BlockStateComponent comp = new BlockStateComponent(BlockStateComponent.DEFAULT.properties());
+                    for (Property<?> prop : state.getProperties()) {
+                        if (!ignoredProperties.contains(prop.getName())) {
+                            comp = comp.with(prop, state);
+                        }
+                    }
+                    return Map.entry(blockItem, comp);
+                })
+                .filter(entry -> !entry.getValue().properties().isEmpty())
+                .distinct()
+                .map(entry -> {
+                    ItemStack stack = new ItemStack(entry.getKey());
+                    stack.set(DataComponentTypes.BLOCK_STATE, entry.getValue());
+
+                    NbtCompound nbtCompound = new NbtCompound();
+                    nbtCompound.putBoolean(MOD_ID + "_tooltip", true);
+
+                    stack.set(DataComponentTypes.CUSTOM_DATA, NbtComponent.of(nbtCompound));
+                    return stack;
+                }).toList();
     }
 
     public static boolean shouldHandleBlock(Identifier blockId) {
