52e4058368c1464177422c2574799cd31c287e79
cargocats <ceggy@protonmail.com>
2025-06-13 20:21:11 -0400

    Merge from 1.21.5

--- src/client/java/com/github/cargocats/illicitblocks/client/IllicitBlocksClient.java
+++ src/client/java/com/github/cargocats/illicitblocks/client/IllicitBlocksClient.java
@@ -1,71 +1,33 @@
 package com.github.cargocats.illicitblocks.client;
 
-import com.github.cargocats.illicitblocks.Utils;
-import com.github.cargocats.illicitblocks.client.api.AdditionalItemAssetRegistrationCallback;
-import com.github.cargocats.illicitblocks.client.api.AdditionalModelRegistrationCallback;
-import com.google.gson.JsonObject;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
-import net.minecraft.block.AbstractFireBlock;
-import net.minecraft.block.AirBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BubbleColumnBlock;
-import net.minecraft.block.CocoaBlock;
-import net.minecraft.block.CropBlock;
-import net.minecraft.block.EndGatewayBlock;
-import net.minecraft.block.EndPortalBlock;
-import net.minecraft.block.FlowerPotBlock;
-import net.minecraft.block.FluidBlock;
-import net.minecraft.block.FrostedIceBlock;
-import net.minecraft.block.LeveledCauldronBlock;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.NetherPortalBlock;
-import net.minecraft.block.PistonExtensionBlock;
-import net.minecraft.block.RedstoneWireBlock;
-import net.minecraft.block.StemBlock;
-import net.minecraft.block.SweetBerryBushBlock;
-import net.minecraft.block.TallPlantBlock;
-import net.minecraft.block.TripwireBlock;
-import net.minecraft.block.WallBannerBlock;
-import net.minecraft.block.WallHangingSignBlock;
-import net.minecraft.block.WallSignBlock;
-import net.minecraft.block.WallSkullBlock;
-import net.minecraft.client.item.ItemAsset;
-import net.minecraft.client.render.item.model.BasicItemModel;
-import net.minecraft.client.render.item.model.SpecialItemModel;
-import net.minecraft.client.render.item.model.special.BannerModelRenderer;
-import net.minecraft.client.render.item.model.special.HeadModelRenderer;
-import net.minecraft.client.render.item.tint.ConstantTintSource;
-import net.minecraft.client.render.item.tint.MapColorTintSource;
-import net.minecraft.client.render.item.tint.TintSource;
-import net.minecraft.client.render.model.json.JsonUnbakedModel;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.BlockStateComponent;
 import net.minecraft.component.type.NbtComponent;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.registry.Registries;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.ColorHelper;
 
-import java.io.StringReader;
-import java.util.ArrayList;
-import java.util.List;
+import static com.github.cargocats.illicitblocks.IllicitBlocks.*;
 
-import static com.github.cargocats.illicitblocks.IllicitBlocks.MOD_ID;
-import static com.github.cargocats.illicitblocks.IllicitBlocks.createdBlockItems;
 
 public class IllicitBlocksClient implements ClientModInitializer {
-    // This is necessary because these mods don't use their own WoodTypes
-    private static final List<String> annoyingMods = List.of("terrestria", "cinderscapes");
-
     @Override
     public void onInitializeClient() {
-        AdditionalItemAssetRegistrationCallback.EVENT.register(context -> createdBlockItems.forEach(id -> addItemAssetForId(context, id)));
-        AdditionalModelRegistrationCallback.EVENT.register(context -> createdBlockItems.forEach(id -> addModelForId(context, id)));
+        ModelLoadingPlugin.register(new IllicitModelPlugin());
+
+        ColorProviderRegistry.ITEM.register((itemStack, tintIndex) -> {
+            Identifier id = Registries.ITEM.getId(itemStack.getItem());
+            return colorMap.getOrDefault(id, -1);
+        }, toBeTinted.toArray(new ItemConvertible[]{}));
 
         // TODO: Cache results?
-        ItemTooltipCallback.EVENT.register((itemStack, context, type, textList) -> {
+        ItemTooltipCallback.EVENT.register((itemStack, context, textList) -> {
             NbtComponent component = itemStack.get(DataComponentTypes.CUSTOM_DATA);
 
             if (component != null && component.contains(MOD_ID + "_tooltip")) {
@@ -79,189 +41,6 @@ public class IllicitBlocksClient implements ClientModInitializer {
                 }
             }
         });
-    }
-
-    public static void addItemAssetForId(AdditionalItemAssetRegistrationCallback.Context context, Identifier id) {
-        if (context.hasAsset(id)) return;
-        Block block = Registries.BLOCK.get(id);
-
-        List<TintSource> tintSources = new ArrayList<>();
-
-        switch (id.toString()) {
-            case "minecraft:water", "minecraft:water_cauldron" -> tintSources.add(new ConstantTintSource(ColorHelper.getArgb(63, 118, 228)));
-            case "minecraft:melon_stem", "minecraft:pumpkin_stem", "minecraft:attached_pumpkin_stem",
-                 "minecraft:attached_melon_stem" -> tintSources.add(new MapColorTintSource(MapColor.get(7).color));
-            case "minecraft:redstone_wire" -> tintSources.add(new MapColorTintSource(MapColor.get(52).color));
-            default -> {}
-        }
-
-        switch (block) {
-            case WallBannerBlock wallBannerBlock -> context.addAsset(id, new ItemAsset(
-                    new SpecialItemModel.Unbaked(
-                            Identifier.ofVanilla("item/template_banner"),
-                            new BannerModelRenderer.Unbaked(wallBannerBlock.getColor())
-                    ),
-                    ItemAsset.Properties.DEFAULT
-            ));
-            case WallSkullBlock wallSkullBlock -> context.addAsset(id, new ItemAsset(
-                    new SpecialItemModel.Unbaked(
-                            Identifier.ofVanilla("item/template_skull"),
-                            new HeadModelRenderer.Unbaked(wallSkullBlock.getSkullType())
-                    ),
-                    ItemAsset.Properties.DEFAULT
-            ));
-            default -> context.addAsset(id, new ItemAsset(
-                    new BasicItemModel.Unbaked(
-                            id,
-                            tintSources
-                    ),
-                    ItemAsset.Properties.DEFAULT
-            ));
-        }
-    }
-
-    public static void addModelForId(AdditionalModelRegistrationCallback.Context context, Identifier id) {
-        if (context.hasModel(id)) return;
-        Block block = Registries.BLOCK.get(id);
-
-        JsonObject jsonRoot = new JsonObject();
-        jsonRoot.addProperty("gui_light", "front");
-
-        switch (block) {
-            case FluidBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", id.getNamespace() + ":block/" + id.getPath() + "_still");
-            }
-            case CropBlock ignored -> jsonRoot.addProperty("parent", "block/" + id.getPath() + "_stage0");
-            case WallSignBlock wallSignBlock -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                String woodName = wallSignBlock.getWoodType().name().replaceFirst(id.getNamespace() + ":", "");
-                if (annoyingMods.contains(id.getNamespace())) {
-                    woodName = extractWoodType(id.toString());
-                }
-
-                String itemTextureString = id.getNamespace() + ":item/" + woodName + "_sign";
-                textures.addProperty("layer0", itemTextureString);
-            }
-            case WallHangingSignBlock wallHangingSignBlock -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                String woodName = wallHangingSignBlock.getWoodType().name().replaceFirst(id.getNamespace() + ":", "");
-                if (annoyingMods.contains(id.getNamespace())) {
-                    woodName = extractWoodType(id.toString());
-                }
-
-                textures.addProperty("layer0", id.getNamespace() + ":item/" + woodName + "_hanging_sign");
-            }
-            case FlowerPotBlock ignored when !id.getNamespace().equals("minecraft") -> {
-                jsonRoot.addProperty("parent", "minecraft:block/flower_pot_cross");
-
-                String texture = id.getPath().replace("potted_", "");
-
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                if (!context.hasModel(Identifier.of(id.getNamespace(), "block/" + texture))) {
-                    textures.addProperty("plant", id.getNamespace() + ":block/" + texture + "_top");
-                } else {
-                    textures.addProperty("plant", id.getNamespace() + ":block/" + texture);
-                }
-            }
-            case TallPlantBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", id.getNamespace() + ":block/" + id.getPath() + "_bottom");
-            }
-            case AbstractFireBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", id.getNamespace() + ":block/" + id.getPath() + "_0");
-            }
-            case StemBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", id.getNamespace() + ":block/" + id.getPath());
-            }
-            case LeveledCauldronBlock ignored -> jsonRoot.addProperty("parent", id.getNamespace() + ":block/" + id.getPath() + "_full");
-            case SweetBerryBushBlock ignored -> jsonRoot.addProperty("parent", "minecraft:block/sweet_berry_bush_stage0");
-            case FrostedIceBlock ignored -> jsonRoot.addProperty("parent", "minecraft:block/frosted_ice_3");
-            case TripwireBlock ignored -> jsonRoot.addProperty("parent", "minecraft:block/tripwire_attached_nsew");
-            case RedstoneWireBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "minecraft:block/redstone_dust_dot");
-            }
-            case BubbleColumnBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "illicitblocks:item/bubble_column");
-            }
-            case CocoaBlock ignored -> jsonRoot.addProperty("parent", "minecraft:block/cocoa_stage2");
-            case NetherPortalBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "minecraft:block/nether_portal");
-            }
-            case AirBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "illicitblocks:item/" + id.getPath());
-            }
-            case PistonExtensionBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "minecraft:block/piston_top");
-            }
-            case EndPortalBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "illicitblocks:item/end_portal");
-            }
-            case EndGatewayBlock ignored -> {
-                jsonRoot.addProperty("parent", "minecraft:item/generated");
-                JsonObject textures = new JsonObject();
-                jsonRoot.add("textures", textures);
-
-                textures.addProperty("layer0", "illicitblocks:item/end_gateway");
-            }
-            default -> {
-                Utils.debugLog("Default back to block for {}, class: {}", id, block.getClass());
-                jsonRoot.addProperty("parent", "block/" + id.getPath());
-            }
-        }
-
-        context.addModel(id, JsonUnbakedModel.deserialize(new StringReader(jsonRoot.toString())));
     }
 
     public static String extractWoodType(String fullId) {
--- src/main/java/com/github/cargocats/illicitblocks/IllicitBlocks.java
+++ src/main/java/com/github/cargocats/illicitblocks/IllicitBlocks.java
@@ -4,6 +4,7 @@ import com.github.cargocats.illicitblocks.item.IllicitBlockItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.component.DataComponentTypes;
@@ -11,6 +12,7 @@ import net.minecraft.component.type.BlockStateComponent;
 import net.minecraft.component.type.NbtComponent;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -18,22 +20,28 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class IllicitBlocks implements ModInitializer {
     public static final String MOD_ID = "illicitblocks";
     public static final Logger LOG = LoggerFactory.getLogger(MOD_ID);
+    public static ArrayList<Identifier> moddedBlocks = new ArrayList<>();
     public static ArrayList<Identifier> createdBlockItems = new ArrayList<>();
+    public static ArrayList<ItemConvertible> toBeTinted = new ArrayList<>();
+    public static final HashMap<Identifier, Integer> colorMap = new HashMap<>();
+
     public static final Set<String> ignoredProperties = Set.of("facing", "horizontal_facing", "vertical_direction", "axis", "rotation", "east", "west", "south", "north");
     public static boolean DEBUG_LOGGING = true;
 
@@ -47,39 +55,86 @@ public class IllicitBlocks implements ModInitializer {
     public void onInitialize() {
         ConfigManager.loadConfig();
         DEBUG_LOGGING = ConfigManager.config.debug;
-
         Registry.register(Registries.ITEM_GROUP, ILLICIT_BLOCKS_ITEM_GROUP_KEY, ILLICIT_BLOCKS_ITEM_GROUP);
+
+        colorMap.put(new Identifier("minecraft", "water"), ColorHelper.Argb.getArgb(63, 118, 228));
+        colorMap.put(new Identifier("minecraft", "redstone_wire"), ColorHelper.Argb.getArgb(189, 48, 49));
+        colorMap.put(new Identifier("minecraft", "attached_melon_stem"), ColorHelper.Argb.getArgb(0, 124, 0));
+        colorMap.put(new Identifier("minecraft", "attached_pumpkin_stem"), ColorHelper.Argb.getArgb(0, 124, 0));
+        colorMap.put(new Identifier("minecraft", "melon_stem"), ColorHelper.Argb.getArgb(0, 124, 0));
+        colorMap.put(new Identifier("minecraft", "pumpkin_stem"), ColorHelper.Argb.getArgb(0, 124, 0));
+
+        ArrayList<IllicitBlockItem> blockItems = registerBlockItems();
+        ItemGroupEvents.modifyEntriesEvent(ILLICIT_BLOCKS_ITEM_GROUP_KEY)
+                .register(group -> blockItems.forEach(blockItem -> {
+                    group.add(blockItem);
+                    generateItemStacksFromBlockItem(blockItem).forEach(group::add);
+                }));
+
         LOG.info("Loaded Illicit Blocks");
     }
 
     public static ArrayList<IllicitBlockItem> registerBlockItems() {
         ArrayList<IllicitBlockItem> collected = new ArrayList<>();
+        AtomicBoolean hasModdedBlocks = new AtomicBoolean(false);
+        AtomicBoolean newBlock = new AtomicBoolean(false);
 
-        RegistryEntryAddedCallback.allEntries(Registries.BLOCK, refBlock -> {
-            Block block = refBlock.value();
-            Identifier id = Registries.BLOCK.getId(block);
-
-            IllicitBlockItem item = tryRegisterBlock(id);
-            if (item != null) {
-                collected.add(item);
+        RegistryEntryAddedCallback.event(Registries.BLOCK).register((ignored, id, block) -> {
+            if ("minecraft".equals(id.getNamespace())) {
+                IllicitBlockItem item = tryRegisterBlock(id);
+                if (item != null) collected.add(item);
+            } else {
+                if (moddedBlocks.contains(id)) {
+                    IllicitBlockItem item = tryRegisterBlock(id);
+                    if (item != null) collected.add(item);
+                } else {
+                    newBlock.set(true);
+                }
+                hasModdedBlocks.set(true);
             }
         });
+
+        Registries.BLOCK.forEach(block -> {
+            Identifier id = Registries.BLOCK.getId(block);
+
+            if ("minecraft".equals(id.getNamespace())) {
+                IllicitBlockItem item = tryRegisterBlock(id);
+                if (item != null) collected.add(item);
+            } else {
+                if (moddedBlocks.contains(id)) {
+                    IllicitBlockItem item = tryRegisterBlock(id);
+                    if (item != null) collected.add(item);
+                } else {
+                    newBlock.set(true);
+                }
+                hasModdedBlocks.set(true);
+            }
+        });
+
+        if (hasModdedBlocks.get() && moddedBlocks.isEmpty() || newBlock.get()) {
+            ItemStack placeholder = new ItemStack(Items.BARRIER);
+            placeholder.set(DataComponentTypes.CUSTOM_NAME, Text.translatable("illicitblocks.placeholder_description"));
+            ItemGroupEvents.modifyEntriesEvent(ILLICIT_BLOCKS_ITEM_GROUP_KEY)
+                    .register(group -> group.add(placeholder));
+        }
 
         return collected;
     }
 
-    private static IllicitBlockItem tryRegisterBlock(Identifier id) {
+    public static IllicitBlockItem tryRegisterBlock(Identifier id) {
         if (!shouldHandleBlock(id)) {
             Utils.debugLog("Do not handle block {}", id);
             return null;
         }
 
         Block block = Registries.BLOCK.get(id);
-        IllicitBlockItem blockStateBlockItem = new IllicitBlockItem(block, new Item.Settings()
-                .registryKey(RegistryKey.of(RegistryKeys.ITEM, id))
-                .useBlockPrefixedTranslationKey());
-
+        IllicitBlockItem blockStateBlockItem = new IllicitBlockItem(block, new Item.Settings());
         Registry.register(Registries.ITEM, id, blockStateBlockItem);
+        createdBlockItems.add(id);
+
+        if (colorMap.containsKey(id)) {
+            toBeTinted.add(blockStateBlockItem);
+        }
 
         Utils.debugLog("Registered block item: {}", id);
         return blockStateBlockItem;
